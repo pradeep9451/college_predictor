b23443eacb885eb3903783bfda7877d2c9aed8ba
Pradeep Yadav <143262589+pradeep9451@users.noreply.github.com>
2026-05-31 20:59:29 +0530

    Update server.js

--- node-backend/server.js
+++ node-backend/server.js
@@ -16,7 +16,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+  "http://localhost:3000",
+  "https://college-frontend-umr4.onrender.com"
+],
   credentials: true,
 }));
 
